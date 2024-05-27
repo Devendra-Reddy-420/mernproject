15281d31d3c473c0fd8aa611c5393fd721dfc365
Devendra-Reddy-420 <136948121+Devendra-Reddy-420@users.noreply.github.com>
2024-05-27 12:47:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,51 +1,56 @@
-// -----------all links ended---------
-const express=require("express")
-const bodyParser=require("body-parser")
-const nodemon=require("nodemon")
-const ejs=require("ejs")
-const mongoose=require("mongoose")
-const bcrypt=require("bcrypt")
-const router=require("./routes")
-const session=require("express-session")
-const mongodbSession=require("connect-mongodb-session")(session)
-// -----------setup started---------
-const app=express()
-app.set('view engine','ejs')
-const path=require("path")
-app.use(express.static(path.join(__dirname,"/public")))
+const express = require("express");
+const bodyParser = require("body-parser");
+const nodemon = require("nodemon");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const router = require("./routes");
+const session = require("express-session");
+const mongodbSession = require("connect-mongodb-session")(session);
 
-app.use(bodyParser.urlencoded({extended:true}))
-var methodOverride = require('method-override')
-app.use(methodOverride('_method'))
+// Setup
+const app = express();
+app.set('view engine', 'ejs');
+const path = require("path");
+app.use(express.static(path.join(__dirname, "/public")));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+var methodOverride = require('method-override');
+app.use(methodOverride('_method'));
+
+// MongoDB connection
+const mongoURI = "mongodb://mongoadmin:secret@mongo:27017/test?authSource=admin&retryWrites=true&w=majority";
 mongoose
-  .connect(
-    "mongodb://127.0.0.1:27017/?retryWrites=true&w=majority"
-  )
+  .connect(mongoURI)
   .then(() => {
     console.log("db connected....");
   })
   .catch((error) => {
     console.error("Error connecting to database:", error);
   });
-  const store= new mongodbSession({
-    uri:'mongodb://127.0.0.1:27017/?retryWrites=true&w=majority',
-    collection:'sessions'
-  })
-  app.use(session({
-    secret:'this is secret project',
-    resave:false,
-    saveUninitialized:false,
-    store:store
-  }))
-  // ----------middle wares---------
-  app.use(function user(req,res,next){
-    res.locals.user=req.session.user
-    next()
-})
 
-// -------use routes-------
-app.use('/',router)
-// -----start server------
-app.listen(4000,()=>{
-    console.log('port running on 4000...')
-})
+const store = new mongodbSession({
+  uri: mongoURI,
+  collection: 'sessions'
+});
+
+app.use(session({
+  secret: 'this is secret project',
+  resave: false,
+  saveUninitialized: false,
+  store: store
+}));
+
+// Middlewares
+app.use(function user(req, res, next) {
+  res.locals.user = req.session.user;
+  next();
+});
+
+// Use routes
+app.use('/', router);
+
+// Start server
+app.listen(4000, () => {
+  console.log('port running on 4000...');
+});
